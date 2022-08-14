eaa20f90d1bec919c00ccc7f7f9699e8d4762cb3
Nuno Silva <nunocaminada@yahoo.com.br>
2022-08-14 01:15:38 -0300

    Update acessaApi.py
    Ainda mexendo no telefone

--- acessaApi.py
+++ acessaApi.py
@@ -2,6 +2,8 @@ import requests
 import json
 import pandas as pd
 import re
+import phonenumbers
+import csv
 
 def jprint(obj):
     # create a formatted string of the Python JSON object
@@ -13,26 +15,22 @@ def phone_format(phone_number):
     formatted_phone_number = re.sub("(\d)(?=(\d{3})+(?!\d))", r"\1-", "%d" % int(clean_phone_number[:-1])) + clean_phone_number[-1]
     return formatted_phone_number
 
-def transformaFormatos(meuDataframe):
-    meuDataframe['cell'] = phone_format(meuDataframe['cell'])
-    return meuDataframe
+def getData(linhas):
+    dados = requests.get("https://randomuser.me/api/?results="+str(linhas)) 
+    return dados
 
-def formatPhone(number):    
-    return('(%s) %s-%s' % tuple(re.findall(r'\d{4}$|\d{3}', number)))
+dados = getData(6)
+dadosJson = dados.json()['results']
 
-response = requests.get("https://randomuser.me/api/?results=3")
-x = response.json()
-df = pd.DataFrame(x['results'])
-#novoDf = transformaFormatos(df)
-#print(df['cell'])
+cellNumber = dadosJson[0]['cell']
+i=0
+for row in dadosJson:
+    cellNumber = dadosJson[i]['cell'] 
+    clean_phone_number = re.sub('[^0-9]+', '', cellNumber)
+    #numero = phonenumbers.parse(cellNumber,"BR")
+    #print()
+    numero = phonenumbers.format_number(clean_phone_number,phonenumbers.PhoneNumberFormat.INTERNATIONAL)
+    print(numero)
+    i+=1
 
-#print(formatPhone(df['cell'].item()))
-print(df['cell'])
-
-#df.to_csv('out.csv')
-
-#pf = pd.DataFrame.from_dict(response.json(), orient="index")
-
-#jprint(response.json()['results'])
-
-#pf.to_csv('out.csv')
+    
