0214184c1fd6e684ab4e8efb67511cde27bbe069
Nuno Silva <nunocaminada@yahoo.com.br>
2022-08-14 21:21:23 -0300

    Na etapa 4

--- acessaApi.py
+++ acessaApi.py
@@ -1,45 +1,51 @@
-import requests
-import json
 import pandas as pd
+import time
 import re
-import phonenumbers
-import csv
-import io
 
-def jprint(obj):
-    # create a formatted string of the Python JSON object
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
+# Dificuldades: 
+# encoding da API, em especial dos dados que não eram BR
+# encoding dos arquivos
+# dualidade csv/json, que apreta diferenças no tratamento que nem sempre ficam claras na documentação
 
-def phone_format(phone_number):
-    clean_phone_number = re.sub('[^0-9]+', '', phone_number)
-    formatted_phone_number = re.sub("(\d)(?=(\d{3})+(?!\d))", r"\1-", "%d" % int(clean_phone_number[:-1])) + clean_phone_number[-1]
-    return formatted_phone_number
-
+#Objetivo: Buscar um número determinado de linhas da API Randomuser em formato CSV
+#Parametro: número de linhas a retornar no dataframe
+#Retorno: Pandas dataframe
 def getData(linhas):
-    #dados = requests.get("https://randomuser.me/api/?results="+str(linhas)).content
-    url = "https://randomuser.me/api/?format=csv&nat=br&results="+str(linhas)
-    meuDataframe = pd.read_csv(url, encoding='latin_1') 
+    linhas +=1 #para compensar o header
+    url = "https://randomuser.me/api/?format=csv&nat=us,br,ca,fr,au&include=name&results="+str(linhas)
+    meuDataframe = pd.read_csv(url, encoding='utf-8')
     return meuDataframe
 
-#def formataPhone(meuDataframe):
-#    for row in meuDataframe:
+#Objetivo: Escrever arquivo com dados da API Randomuser
+#Parametro: Pandas dataframe
+#Retorno: nenhum
+def escreveResultado(meuDataframe):
+    timestampArquivo = time.strftime("%Y%m%d-%H%M%S")
+    #print(meuDataframe.head())
+    meuDataframe.to_csv('.\\Saida\\'+timestampArquivo+".csv",sep=";",encoding='ansi')
 
+#Objetivo: Converter numeros de celular
+#Parametro: Pandas dataframe
+#Retorno: Pandas dataframe
+def transformaCelulares(meuDataframe):
+    meuDataframe['cell'] = meuDataframe['cell'].apply(limpaNumero)
+    return meuDataframe
 
-dataframeNomes = getData(10)
-print(dataframeNomes["cell"])
+#Objetivo: Limpar caracteres de uma string, deixando apenas numeros
+#Parametro: string
+#Retorno: numero
+def limpaNumero(numero):
+    return (re.sub('[^0-9]+', '', numero))
 
+def calculaPorcentagem(meuDataframe):
+    quantidadePaises = meuDataframe['location.country'].value_counts()
+    print(quantidadePaises)
 
-#dadosJson = dados.json()['results']
+dataframeNomes = getData(600)
+cellLimpos = transformaCelulares(dataframeNomes)
 
-#cellNumber = dadosJson[0]['cell']
-#i=0
-#for row in dadosJson:
-#    cellNumber = dadosJson[i]['cell'] 
-#    clean_phone_number = re.sub('[^0-9]+', '', cellNumber)
+escreveResultado(cellLimpos)
 
-#    numero = phonenumbers.format_number(clean_phone_number,phonenumbers.PhoneNumberFormat.INTERNATIONAL)
-#    print(numero)
-#    i+=1
-
+print(cellLimpos['location.country'].describe(include='all'))
+print(calculaPorcentagem(cellLimpos))
     
