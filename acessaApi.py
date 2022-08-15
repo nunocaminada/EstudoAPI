c39f32018e8d69f631f2b7c41f4520fbb9f55bb6
Nuno Silva <nunocaminada@yahoo.com.br>
2022-08-14 23:12:02 -0300

    Update acessaApi.py
    Porcentagens corretamente exibidas

--- acessaApi.py
+++ acessaApi.py
@@ -13,6 +13,7 @@ import re
 def getData(linhas):
     linhas +=1 #para compensar o header
     url = "https://randomuser.me/api/?format=csv&nat=us,br,ca,fr,au&include=name&results="+str(linhas)
+    #le da API e coloca na codificação UTF-8
     meuDataframe = pd.read_csv(url, encoding='utf-8')
     return meuDataframe
 
@@ -20,8 +21,9 @@ def getData(linhas):
 #Parametro: Pandas dataframe
 #Retorno: nenhum
 def escreveResultado(meuDataframe):
+    #monta um timestamp para dar nome ao arquivo
     timestampArquivo = time.strftime("%Y%m%d-%H%M%S")
-    #print(meuDataframe.head())
+    #grava um arquivo diferente a cada execução
     meuDataframe.to_csv('.\\Saida\\'+timestampArquivo+".csv",sep=";",encoding='ansi')
 
 #Objetivo: Converter numeros de celular
@@ -37,15 +39,28 @@ def transformaCelulares(meuDataframe):
 def limpaNumero(numero):
     return (re.sub('[^0-9]+', '', numero))
 
+#Objetivo: Imprimir percentual de pessoas por pais e por genero
+#Parametro: Dataframe
+#Retorno: nenhum
 def calculaPorcentagem(meuDataframe):
-    quantidadePaises = meuDataframe['location.country'].value_counts()
-    print(quantidadePaises)
+    #calcula as porcentagens e formata a saída
+    porcentagemPaises = ((meuDataframe['location.country'].value_counts()/meuDataframe['location.country'].count())*100).map('{:,.2f}%'.format)
+    porcentagemGeneros = ((meuDataframe['gender'].value_counts()/meuDataframe['gender'].count())*100).map('{:,.2f}%'.format)
+    #imprime os resultados
+    print("Distribuicao percentual por pais")
+    print(porcentagemPaises)
+    print("\nDistribuicao percentual por genero")
+    print(porcentagemGeneros)
 
-dataframeNomes = getData(600)
-cellLimpos = transformaCelulares(dataframeNomes)
 
+#acessa a API e monta o dataframe
+dataframeOriginal = getData(600)
+#formata os numeros de celular
+cellLimpos = transformaCelulares(dataframeOriginal)
+
+#Grava em arquivo CSV
 escreveResultado(cellLimpos)
 
-print(cellLimpos['location.country'].describe(include='all'))
+#Imprime os calculos de porcentagem
 print(calculaPorcentagem(cellLimpos))
     
